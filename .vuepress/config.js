96adb49251dbf3e3eee2931d44946036464c507a
Ray <ray@Rayde-MacBook-Pro.local>
2021-04-08 16:41:00 +0800

    add config.js

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "VuePress Blog",
+  description: "個人部落格",
+  head: [
+    ['meta', { name: "viewport", content: "width=device-width,user-scaleble=0,initial-scale=1.0,maximum-scale=1.0" }],
+  ],
+  locales: {
+    '/': {
+      lang: 'zh-TW',
+    }
+  },
+};
